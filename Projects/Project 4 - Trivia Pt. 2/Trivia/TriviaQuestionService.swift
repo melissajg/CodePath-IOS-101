34aed4f635ef563d6f26e4819f547f86cc8624d8
melissajg <42328365+melissajg@users.noreply.github.com>
2023-10-05 20:54:39 -0700

    fixing triviaQuestionService

--- Projects/Project 4 - Trivia Pt. 2/Trivia/TriviaQuestionService.swift
+++ Projects/Project 4 - Trivia Pt. 2/Trivia/TriviaQuestionService.swift
@@ -10,8 +10,7 @@ class TriviaQuestionService {
     static func fetchTrivia(amount: Int,
                             completion: ((TriviaQuestion) -> Void)? = nil) {
         let parameters = "amount=\(amount)"
-      //      let url = URL(string: "https://api.open-meteo.com/v1/forecast?\(parameters)")!
-        let url = URL(string: "https://opentdb.com/api.php?amount=10")!
+        let url = URL(string: "https://opentdb.com/api.php?\(parameters)")!
             // create a data task and pass in the URL
             let task = URLSession.shared.dataTask(with: url) { data, response, error in
               // this closure is fired when the response is received
@@ -36,23 +35,23 @@ class TriviaQuestionService {
             task.resume() // resume the task and fire the request
 
     }
-    /*
+    
       private static func parse(data: Data) -> TriviaQuestion {
           // transform the data we received into a dictionary [String: Any]
           let jsonDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-          let currentQuestion = jsonDictionary["current_weather"] as! [String: Any]
-          // wind speed
-          let windSpeed = currentWeather["windspeed"] as! Double
-          // wind direction
-          let windDirection = currentWeather["winddirection"] as! Double
-          // temperature
-          let temperature = currentWeather["temperature"] as! Double
-          // weather code
-          let weatherCodeRaw = currentWeather["weathercode"] as! Int
-          return TriviaQuestion(windSpeed: windSpeed,
-                                        windDirection: windDirection,
-                                        temperature: temperature,
-                                        weatherCodeRaw: weatherCodeRaw)
+          let currentQuestion = jsonDictionary["current_question"] as! [String: Any]
+          // category
+          let category = currentQuestion["category"] as! String
+          // question
+          let question = currentQuestion["question"] as! String
+          // correctAnswer
+          let correctAnswer = currentQuestion["correct_answer"] as! String
+          // incorrectAnswers
+          let incorrectAnswers = currentQuestion["incorrect_answers"] as! [String]
+          return TriviaQuestion(category: category,
+                                question: question,
+                                correctAnswer: correctAnswer,
+                                incorrectAnswers: incorrectAnswers)
       }
-     */
+     
   }
