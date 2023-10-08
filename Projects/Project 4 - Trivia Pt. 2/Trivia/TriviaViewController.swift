4365958fa9c5c79abc8faf4d46ed8b37690d6a12
melissajg <42328365+melissajg@users.noreply.github.com>
2023-10-08 14:50:04 -0700

    Porject 4 Completely done
    all code done need notes

--- Projects/Project 4 - Trivia Pt. 2/Trivia/TriviaViewController.swift
+++ Projects/Project 4 - Trivia Pt. 2/Trivia/TriviaViewController.swift
@@ -6,9 +6,7 @@
 //
 
 import UIKit
-struct Question {
-    let amount : Int
-}
+
 class TriviaViewController: UIViewController {
   
   @IBOutlet weak var currentQuestionNumberLabel: UILabel!
@@ -20,33 +18,28 @@ class TriviaViewController: UIViewController {
   @IBOutlet weak var answerButton2: UIButton!
   @IBOutlet weak var answerButton3: UIButton!
     private var correctAnswersList: [String] = []
-  private var questionAmount = 10
-  private var questions = [Question]()
-  private var currQuestionIndex = 0
-  private var numCorrectQuestions = 0
+    private var questionAmount: Int = 5
+  private var currQuestionIndex: Int = 0
+  private var numCorrectQuestions: Int = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
     addGradient()
     questionContainerView.layer.cornerRadius = 8.0
-    questions = [Question(amount: questionAmount)]
     updateQuestion(withQuestionIndex: 0)
   }
     private func updateQuestion(withQuestionIndex questionIndex: Int) {
         guard questionIndex < questionAmount else { return }
-        TriviaQuestionService.fetchTrivia(amount: questionAmount) {
+        TriviaQuestionService.fetchTrivia(amount: 1) {
             trivia in self.configure(with: trivia, withQuestionIndex: questionIndex)
             }
     }
     private func configure(with trivia: TriviaQuestion, withQuestionIndex questionIndex: Int) {
-        
         currentQuestionNumberLabel.text = "Question: \(questionIndex + 1)/\(questionAmount)"
-        //let question = questions[questionIndex]
-        questionLabel.text = trivia.question
-        categoryLabel.text = trivia.category
+        questionLabel.text = trivia.question.htmlDecoded
+        categoryLabel.text = trivia.category.htmlDecoded
           // combine array of answers (right and wrong) and shuffle them
         let answers = ([trivia.correct_answer] + trivia.incorrect_answers).shuffled()
-        print("Nunber of Questions", answers.count)
         correctAnswersList.append(trivia.correct_answer)
         // made all the button hidden
         answerButton0.isHidden = true
@@ -55,22 +48,22 @@ class TriviaViewController: UIViewController {
         answerButton3.isHidden = true
         // if theres atleast one answer show first button
         if answers.count > 0 {
-            answerButton0.setTitle(answers[0], for: .normal)
+            answerButton0.setTitle(answers[0].htmlDecoded, for: .normal)
             answerButton0.isHidden = false
         }
           // if theres atleast 2 answers show first two buttons
         if answers.count > 1 {
-            answerButton1.setTitle(answers[1], for: .normal)
+            answerButton1.setTitle(answers[1].htmlDecoded, for: .normal)
           answerButton1.isHidden = false
         }
           // if theres atleast one answer show first three buttons
         if answers.count > 2 {
-            answerButton2.setTitle(answers[2], for: .normal)
+            answerButton2.setTitle(answers[2].htmlDecoded, for: .normal)
           answerButton2.isHidden = false
         }
           // if theres atleast one answer show all four buttons
         if answers.count > 3 {
-            answerButton3.setTitle(answers[3], for: .normal)
+            answerButton3.setTitle(answers[3].htmlDecoded, for: .normal)
           answerButton3.isHidden = false
         }
         
@@ -114,23 +107,26 @@ class TriviaViewController: UIViewController {
   }
   
     private func isCorrectAnswer(_ correctAnswersList: [String], _ currQuestionIndex: Int, _ chosenAnswer: String) -> Bool {
-        print("Chosen answer: \(chosenAnswer), Correct answer: \(correctAnswersList.last ?? "EMPTY")")
+        print("Chosen answer: \(chosenAnswer), Correct answer: \(correctAnswersList[currQuestionIndex])")
         print("correctAnswers full: ", correctAnswersList)
-        return chosenAnswer == correctAnswersList[currQuestionIndex+1]
+        if ( correctAnswersList.count - 1 >= currQuestionIndex) {
+            return chosenAnswer == correctAnswersList[currQuestionIndex]
+        }
+        else{
+            return false
+        }
   }
 
   
   private func showFinalScore() {
-      print("correctAnswers full: ", correctAnswersList)
     let alertController = UIAlertController(title: "Game over!",
                                             message: "Final score: \(numCorrectQuestions)/\(questionAmount)",
                                             preferredStyle: .alert)
     let resetAction = UIAlertAction(title: "Restart", style: .default) { [unowned self] _ in
-      currQuestionIndex = 0
-      numCorrectQuestions = 0
+        currQuestionIndex = 0
+        numCorrectQuestions = 0
         correctAnswersList.removeAll()
-        print("correctAnswer empty: ", correctAnswersList)
-      updateQuestion(withQuestionIndex: currQuestionIndex)
+        updateQuestion(withQuestionIndex: currQuestionIndex)
     }
     alertController.addAction(resetAction)
     present(alertController, animated: true, completion: nil)
@@ -162,7 +158,16 @@ class TriviaViewController: UIViewController {
       updateToNextQuestion(sender.titleLabel?.text ?? "")
   }
     
- 
      
 }
+// https://stackoverflow.com/questions/25607247/how-do-i-decode-html-entities-in-swift
+extension String {
+    var htmlDecoded: String {
+        let decoded = try? NSAttributedString(data: Data(utf8), options: [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ], documentAttributes: nil).string
 
+        return decoded ?? self
+    }
+}
